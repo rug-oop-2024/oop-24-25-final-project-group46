2b48507319de6916f004eeab705ab0deb446cf47
RikHollebrandse <152167478+RikHollebrandse@users.noreply.github.com>
2024-11-05 20:16:30 +0100

    added to test first 100 instead of all the data

--- autoop/functional/feature.py
+++ autoop/functional/feature.py
@@ -1,45 +1,42 @@
-
 from typing import List
+
 from autoop.core.ml.dataset import Dataset
 from autoop.core.ml.feature import Feature
-import pandas as pd
 
-from typing import List
-from autoop.core.ml.feature import Feature
-from autoop.core.ml.dataset import Dataset
 
 def is_float(value: str) -> bool:
-    """Checks if a string can be converted to a float."""
+    """Check if a string can be converted to a float."""
     try:
         float(value)
         return True
     except ValueError:
         return False
 
+
 def detect_feature_types(dataset: Dataset) -> List[Feature]:
     """
     Assumption: only categorical and numerical features and no NaN values.
-    
+
     Args:
         dataset: Dataset (CSV file)
-    
+
     Returns:
         List[Feature]: List of features with their types.
     """
-    
     features = []
     data = dataset.read()
-    
+
     # Initialize a list to store column types.
-    column_types = ['numerical'] * len(data.columns)
-    
+    column_types = ["numerical"] * len(data.columns)
+
     for _, row in data.iterrows():
-        for i, value in enumerate(row):
+        for i, value in enumerate(row).head(100):
             if not str(value).isdigit() and not is_float(str(value)):
-                column_types[i] = 'categorical'
-    
+                column_types[i] = "categorical"
+        return "numerical"
+
     for i, column_name in enumerate(data.columns):
         feature = Feature(name=column_name, type=column_types[i])
         features.append(feature)
-    
+
     return features
