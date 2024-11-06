acd47f4a126cc345c4f9a22b60b0668774a7be29
MaiteBoelen <m.c.j.m.boelen@student.rug.nl>
2024-11-06 21:26:16 +0100

    commenting out imports, change it for later use

--- autoop/core/ml/model/classification/__init__.py
+++ autoop/core/ml/model/classification/__init__.py
@@ -1,7 +1,7 @@
 """Create a constructor for the classification models."""
-from autoop.core.ml.model.classification.decision_tree_class import (
-    DecisionTreeClassification,
-)
-from autoop.core.ml.model.classification.knn import KNN
-from autoop.core.ml.model.classification.svc import SupportVectorClassification
+# from autoop.core.ml.model.classification.decision_tree_class import (
+#     DecisionTreeClassification,
+# )
+# from autoop.core.ml.model.classification.knn import KNN
+# from autoop.core.ml.model.classification.svc import SupportVectorClassification
 
