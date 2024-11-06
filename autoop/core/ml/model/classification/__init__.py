162d79dcde83266eba5f0a4312221faf5181b2b4
RikHollebrandse <152167478+RikHollebrandse@users.noreply.github.com>
2024-11-06 22:55:37 +0100

    style changes

--- autoop/core/ml/model/classification/__init__.py
+++ autoop/core/ml/model/classification/__init__.py
@@ -1,6 +1,10 @@
 """Create a constructor for the classification models."""
-from autoop.core.ml.model.classification.decision_tree_class import (
-    DecisionTreeClassification,
-)
+from autoop.core.ml.model.classification.dtc import DecisionTreeClassification
 from autoop.core.ml.model.classification.knn import KNN
 from autoop.core.ml.model.classification.svc import SupportVectorClassification
+
+__all__ = [
+    "DecisionTreeClassification",
+    "KNN",
+    "SupportVectorClassification",
+]
