f42546b2b7ff177b27e103324f0e79c6f8184046
MaiteBoelen <m.c.j.m.boelen@student.rug.nl>
2024-11-06 15:00:16 +0100

    altered the init

--- autoop/core/ml/model/__init__.py
+++ autoop/core/ml/model/__init__.py
@@ -1,23 +1,15 @@
-from autoop.core.ml.model.classification.decision_tree_class import (
-    DecisionTreeClassification,
-)
-from autoop.core.ml.model.classification.knn import KNN
-from autoop.core.ml.model.classification.svc import SupportVectorClassification
 from autoop.core.ml.model.model import Model
-from autoop.core.ml.model.regression.decision_tree_regr import DecisionTreeRegression
-from autoop.core.ml.model.regression.mlr import MultipleLinearRegression
-from autoop.core.ml.model.regression.svm import SVR
 
 REGRESSION_MODELS = [
-    DecisionTreeRegression,
-    MultipleLinearRegression,
-    SVR,
+    "DecisionTreeRegression",
+    "MultipleLinearRegression",
+    "SVR",
 ]  # add your models as str here
 
 CLASSIFICATION_MODELS = [
-    DecisionTreeClassification,
-    KNN,
-    SupportVectorClassification,
+    "DecisionTreeClassification",
+    "KNN",
+    "SupportVectorClassification",
 ]  # add your models as str here
 
 
