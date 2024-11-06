046782e180e914a6b0ca6fa4800e4c5d108d9114
RikHollebrandse <152167478+RikHollebrandse@users.noreply.github.com>
2024-11-06 21:31:19 +0100

    style changes

--- autoop/core/ml/model/regression/__init__.py
+++ autoop/core/ml/model/regression/__init__.py
@@ -1,5 +1,4 @@
-
-from autoop.core.ml.model.regression.decision_tree_regr import DecisionTreeRegression
+from autoop.core.ml.model.regression.dtr import DecisionTreeRegression
 from autoop.core.ml.model.regression.mlr import MultipleLinearRegression
 from autoop.core.ml.model.regression.svm import SupportVectorRegression
 
