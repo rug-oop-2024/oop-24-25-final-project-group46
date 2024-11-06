7dd84786fa12799d9c51d564af488090606b738a
MaiteBoelen <m.c.j.m.boelen@student.rug.nl>
2024-11-06 13:50:50 +0100

    added pipeline

--- autoop/core/ml/pipeline.py
+++ autoop/core/ml/pipeline.py
@@ -1,25 +1,28 @@
-from typing import List
 import pickle
+from typing import List
 
 from autoop.core.ml.artifact import Artifact
 from autoop.core.ml.dataset import Dataset
-from autoop.core.ml.model import Model
 from autoop.core.ml.feature import Feature
 from autoop.core.ml.metric import Metric
+from autoop.core.ml.model import Model
 from autoop.functional.preprocessing import preprocess_features
+
+
 import numpy as np
 
 
-class Pipeline():
-    
-    def __init__(self, 
-                 metrics: List[Metric],
-                 dataset: Dataset, 
-                 model: Model,
-                 input_features: List[Feature],
-                 target_feature: Feature,
-                 split=0.8,
-                 ): # Add return type 
+class Pipeline:
+    """A class for the entire pipeline of the ML models."""
+    def __init__(
+        self,
+        metrics: List[Metric],
+        dataset: Dataset,
+        model: Model,
+        input_features: List[Feature],
+        target_feature: Feature,
+        split=0.8,
+    ):  # Add return type
         """Create a constructor for the pipeline class."""
         self._dataset = dataset
         self._model = model
@@ -29,9 +32,13 @@ class Pipeline():
         self._artifacts = {}
         self._split = split
         if target_feature.type == "categorical" and model.type != "classification":
-            raise ValueError("Model type must be classification for categorical target feature")
+            raise ValueError(
+                "Model type must be classification for categorical target feature"
+            )
         if target_feature.type == "continuous" and model.type != "regression":
-            raise ValueError("Model type must be regression for continuous target feature")
+            raise ValueError(
+                "Model type must be regression for continuous target feature."
+            )
 
     def __str__(self):
         """Create a method for returning the pipeline."""
@@ -52,8 +59,7 @@ Pipeline(
 
     @property
     def artifacts(self) -> List[Artifact]:
-        """Used to get the artifacts generated during the pipeline execution to be saved
-        """
+        """Get artifacts generated during the pipeline execution to be saved."""
         artifacts = []
         for name, artifact in self._artifacts.items():
             artifact_type = artifact.get("type")
@@ -70,20 +76,27 @@ Pipeline(
             "target_feature": self._target_feature,
             "split": self._split,
         }
-        artifacts.append(Artifact(name="pipeline_config", data=pickle.dumps(pipeline_data)))
-        artifacts.append(self._model.to_artifact(name=f"pipeline_model_{self._model.type}"))
+        artifacts.append(
+            Artifact(name="pipeline_config", data=pickle.dumps(pipeline_data))
+        )
+        artifacts.append(
+            self._model.to_artifact(name=f"pipeline_model_{self._model.type}")
+        )
         return artifacts
-    
-    def _register_artifact(self, name: str, artifact): # Add return type
+
+    def _register_artifact(self, name: str, artifact):  # Add return type
         """Create a method for registering an artifcact."""
         self._artifacts[name] = artifact
 
-    def _preprocess_features(self): # Add return type
+    def _preprocess_features(self):  # Add return type
         """Create a method for preprocessing the features."""
-        (target_feature_name, target_data, artifact) = preprocess_features([self._target_feature], self._dataset)[0]
+        (target_feature_name, target_data, artifact) = preprocess_features(
+            [self._target_feature], self._dataset
+        )[0]
         self._register_artifact(target_feature_name, artifact)
-        input_results = preprocess_features(self._input_features, self._dataset)
-        for (feature_name, data, artifact) in input_results:
+        input_results = preprocess_features(
+            self._input_features, self._dataset)
+        for feature_name, data, artifact in input_results:
             self._register_artifact(feature_name, artifact)
         # Get the input vectors and output vector, sort by feature name for consistency
         self._output_vector = target_data
@@ -93,22 +106,26 @@ Pipeline(
         """Create a method for splitting the data into training and testing sets."""
         # Split the data into training and testing sets
         split = self._split
-        self._train_X = [vector[:int(split * len(vector))] for vector in self._input_vectors]
-        self._test_X = [vector[int(split * len(vector)):] for vector in self._input_vectors]
-        self._train_y = self._output_vector[:int(split * len(self._output_vector))]
-        self._test_y = self._output_vector[int(split * len(self._output_vector)):]
+        self._train_X = [
+            vector[: int(split * len(vector))] for vector in self._input_vectors
+        ]
+        self._test_X = [
+            vector[int(split * len(vector)) :] for vector in self._input_vectors
+        ]
+        self._train_y = self._output_vector[: int(split * len(self._output_vector))]
+        self._test_y = self._output_vector[int(split * len(self._output_vector)) :]
 
     def _compact_vectors(self, vectors: List[np.array]) -> np.array:
         """Returns combined vectors."""
         return np.concatenate(vectors, axis=1)
 
-    def _train(self): # Add return type
+    def _train(self) -> None:
         """Create a method for training the data."""
         X = self._compact_vectors(self._train_X)
         Y = self._train_y
         self._model.fit(X, Y)
 
-    def _evaluate(self): # Add return type
+    def _evaluate(self) -> None:
         """Evaluate the model."""
         X = self._compact_vectors(self._test_X)
         Y = self._test_y
@@ -119,16 +136,33 @@ Pipeline(
             self._metrics_results.append((metric, result))
         self._predictions = predictions
 
-    def execute(self): # Add return type
+    def execute(self):
         """Create a method for executing the previous methods."""
         self._preprocess_features()
         self._split_data()
         self._train()
-        self._evaluate()
-        return {
-            "metrics": self._metrics_results,
-            "predictions": self._predictions,
-        }
+                
+        train_X = self._compact_vectors(self._train_X)
+        train_y = self._train_y
+        train_predictions = self._model.predict(train_X)
         
-
-    
+        train_metrics_results = []
+        for metric in self._metrics:
+            result = metric.evaluate(train_y, train_predictions)
+            train_metrics_results.append((metric, result))
+        
+        test_X = self._compact_vectors(self._test_X)
+        test_y = self._test_y
+        test_predictions = self._model.predict(test_X)
+        
+        test_metrics_results = []
+        for metric in self._metrics:
+            result = metric.evaluate(test_y, test_predictions)
+            test_metrics_results.append((metric, result))
+        
+        return {
+            "train_metrics": train_metrics_results,
+            "train_predictions": train_predictions,
+            "test_metrics": test_metrics_results,
+            "test_predictions": test_predictions,
+        }
